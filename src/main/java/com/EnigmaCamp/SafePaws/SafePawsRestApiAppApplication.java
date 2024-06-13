38b5f8e7509789a232278f35b554878aaf8edc16
Andhika Pranadipa <andhikapranadipa@gmail.com>
2024-06-13 11:33:45 +0700

    Initial commit

---
+++ src/main/java/com/EnigmaCamp/SafePaws/SafePawsRestApiAppApplication.java
@@ -0,0 +1,13 @@
+package com.EnigmaCamp.SafePaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafePawsRestApiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafePawsRestApiAppApplication.class, args);
+	}
+
+}
